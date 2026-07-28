e0ac1c72a5e1c2670cf732f1b41d83a0db0f1b9e
kotagiri.preeti <kotagiri.preeti19@gmail.com>
2026-07-28 10:13:14 +0530

    mes1

---
+++ Practice/M01_Sliding_Window_&_Two_Pointers/S04/Longest_Substring_Problems.py
@@ -0,0 +1,11 @@
+'''
+Variable sliding window Problems:
+Array based:
+209. Minimum Size Subarray Sum
+713. Subarray product less than K
+904. Fruit Into Baskets
+
+Longest substring based:
+3. Longest Substring Without Repeating Characters
+424. Longest Repeating Character Replacement
+'''
