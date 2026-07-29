9aefbb1e527cfc0b4f58c48f96456659f4be0834
preeti-saarthak <preeti@saarthak.net>
2026-07-29 13:18:20 +0530

    mo1

--- Practice/M01_Sliding_Window_&_Two_Pointers/S03/Variable_Sliding_Window.py
+++ Practice/M01_Sliding_Window_&_Two_Pointers/S03/Variable_Sliding_Window.py
@@ -7,35 +7,33 @@ Array based:
 '''
 from typing import List
 def minSubArrayLen(target: int, nums: List[int]) -> int:
-        left = 0
-        min_len = float("inf")
-        cur_sum = 0
-        for right in range(len(nums)):
-            cur_sum += nums[right]
-            while cur_sum >= target:
-                min_len = min(min_len,right-left+1)
-                cur_sum -= nums[left]
-                left += 1
-        return 0 if min_len == float("inf") else min_len
+    left = 0
+    min_len = float("inf")
+    curr_sum = 0
+    for right in range(len(nums)):
+        curr_sum += nums[right]
+        while curr_sum >= target:
+            min_len = min(min_len,right - left + 1) 
+            curr_sum -= nums[left]
+            left += 1
+    return 0 if min_len == float("inf") else min_len  
+
 target = 7
 nums = [2,3,1,2,4,3]
 print(minSubArrayLen(target,nums))
 
 def numSubarrayProductLessThanK(nums: List[int], k: int) -> int:
-        if k <= 1:
-            return 0
-        left = 0
-        c = 0
-        p = 1
-        for right in range(len(nums)):
-            p *= nums[right]
-            while p >= k:
-                p //= nums[left]
-                left += 1
-            c += (right-left+1)
-        return c
+    if k <= 1:
+        return 0
+    left,count = 0,0
+    p = 1
+    for right in range(len(nums)):
+        p *= nums[right]
+        while p >= k:
+            p //= nums[left]
+            left += 1
+        count += (right - left + 1)
+    return count
 nums = [10,5,2,6]
 k = 100
 print(numSubarrayProductLessThanK(nums,k))
-
-        
